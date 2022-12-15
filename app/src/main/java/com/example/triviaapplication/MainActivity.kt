7e70fc2d5c964cb2943c24a8ff2fbce6cabf2b31
doroteea <doroteeasandor@yahoo.com>
2022-12-15 21:08:50 +0200

    animation added

--- app/src/main/java/com/example/triviaapplication/MainActivity.kt
+++ app/src/main/java/com/example/triviaapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.triviaapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +31,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val questionNumber = questionString.toInt()
 
         if (questionNumber in 1..100){
-            errorMessage.text = "OK"
+            showGIF()
+            Handler().postDelayed(Runnable {
+                clearGif()
+            }, 4000)
         } else {
             errorMessage.text = "Question number should be between 1 and 100"
         }
 
     }
+
+    fun showGIF() {
+        val imageView: ImageView = findViewById(R.id.loadingGif1)
+        Glide.with(this).load(R.drawable.loading2).into(imageView)
+    }
+
+    fun clearGif() {
+        val imageView: ImageView = findViewById(R.id.loadingGif1)
+        Glide.with(this).clear(imageView)
+    }
 }
+
